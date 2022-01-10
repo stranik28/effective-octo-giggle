8c5d1590c0a9378f98dc20fbeb62510cadb6088d
stranik28 <jb357506@gmail.com>
2022-01-10 23:12:59 +0300

    slider add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, {useState} from "react";
 import Modal from "./Modal/Modal";
+import Slider from "./Slider/Slider";
 function App() {
     const [modalActive,setModalActive] = useState(false)
   return (
       <div>
           <button className="open-pop-up" onClick={() => setModalActive(true)}>Нажми здесь</button>
           <Modal active={modalActive} setActive={setModalActive}/>
+          <Slider/>
       </div>
   );
 }
