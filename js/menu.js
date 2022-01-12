37de83eaf7e1cc3aaa60c0cfe192cd2565cb1940
95862888 <astashkin.kirill2002@yandex.ru>
2022-01-12 23:34:51 +0300

    add FAQ section html+css+js

--- js/menu.js
+++ js/menu.js
@@ -14,6 +14,7 @@
         // Toggle open and close nav styles on click
         $('#nav-toggle').click(function() {
             $('nav ul').slideToggle();
+            $('nav>ul').css({'position' : 'absolute', 'bottom' : '0'});
         });
         // Hamburger to X toggle
         $('#nav-toggle').on('click', function() {
